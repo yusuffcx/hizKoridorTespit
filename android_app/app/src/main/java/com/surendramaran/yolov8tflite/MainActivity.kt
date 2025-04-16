b28904fcebd1709a467c2424924173b5e65f4ab6
Yusuf Çokal <134581731+yusuffcx@users.noreply.github.com>
2025-04-16 17:42:40 +0300

    init commit

---
+++ android_app/app/src/main/java/com/surendramaran/yolov8tflite/MainActivity.kt
@@ -0,0 +1,25 @@
+
+package com.surendramaran.yolov8tflite
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.surendramaran.yolov8tflite.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Hız Koridoru Tespit butonuna tıklama olayını ekleyelim
+        binding.speedCameraDetectionButton.setOnClickListener {
+            // DetectionActivity'yi başlat
+            val intent = Intent(this, DetectionActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
